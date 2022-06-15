526ad080d11c1151be124e87f2ce94cddf129b7c
Raymond TeHok <rth@mydatamodels.com>
2022-06-15 21:52:41 +0200

    Add https://leetcode.com/problems/delete-operation-for-two-strings

---
+++ leetcode/583_delete_operation_for_two_strings.py
@@ -0,0 +1,21 @@
+class Solution:
+    # Use Longest Common Subsequence
+    def minDistance(self, word1: str, word2: str) -> int:
+        m = len(word1)
+        n = len(word2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(m + 1):
+            for j in range(n + 1):
+                if i == 0 or j == 0:
+                    continue
+                elif word1[i - 1] == word2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+        return m + n - 2 * dp[m][n]
+
+
+if __name__ == "__main__":
+    assert Solution().minDistance("sea", "eat") == 2
+    assert Solution().minDistance("leetcode", "etco") == 4
