48aa7220f7bafafa0710d2000a7a6e65317dc1fc
rtehok <tehok.raymond@gmail.com>
2022-07-03 09:48:23 +0200

    Update https://leetcode.com/problems/number-of-1-bits

--- leetcode/191_number_of_1_bits.py
+++ leetcode/191_number_of_1_bits.py
@@ -4,13 +4,27 @@ class Solution:
         # return sum([(n & (1 << i)) != 0 for i in range(32)])
         res = 0
         while n != 0:
-            res += n & 1
-            n = n >> 1
-            # print(n, "{0:b}".format(n))
+            res += n & 1  # count if rightmost digit is 1
+            n = n >> 1  # shift right
         return res
 
 
 if __name__ == "__main__":
-    print(Solution().hammingWeight(5))
-    print(Solution().hammingWeight(12))
-    print(Solution().hammingWeight(42))
+    #   101
+    # & 001
+    # -----
+    #   001
+
+    #   10
+    # & 01
+    # ----
+    #   00
+
+    #   1
+    # & 1
+    # ----
+    #   1
+    assert Solution().hammingWeight(5) == 2
+    assert Solution().hammingWeight(12) == 2
+    assert Solution().hammingWeight(42) == 3
+    assert Solution().hammingWeight(11) == 3
