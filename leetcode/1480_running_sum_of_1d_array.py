ad5c3a2b5502c119f6ac04d2a1c246c92bd54577
rtehok <tehok.raymond@gmail.com>
2022-07-05 15:14:49 +0200

    Add https://leetcode.com/problems/running-sum-of-1d-array

---
+++ leetcode/1480_running_sum_of_1d_array.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        for i in range(1, len(nums)):
+            nums[i] += nums[i - 1]
+
+        return nums
+
+
+if __name__ == "__main__":
+    assert Solution().runningSum([1, 2, 3, 4]) == [1, 3, 6, 10]
