c55d720e18998aefad64e89c0dd5aad017b88875
Raymond TeHok <rth@mydatamodels.com>
2022-07-08 12:10:35 +0200

    Add https://leetcode.com/problems/linked-list-cycle-ii

---
+++ leetcode/142_linked_list_cycle_ii.py
@@ -0,0 +1,37 @@
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        node_map = set()
+
+        while head:
+            if head in node_map:
+                return head
+
+            node_map.add(head)
+            head = head.next
+
+        return head
+
+
+if __name__ == "__main__":
+    head = ListNode(1)
+    t = ListNode(2)
+    head.next = t
+    head.next.next = ListNode(3)
+    head.next.next.next = head.next
+    assert Solution().detectCycle(head) == t
+
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    assert Solution().detectCycle(head) is None
+
+
