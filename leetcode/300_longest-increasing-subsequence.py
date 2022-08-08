944c50981ffadc448e4993fe2ab12533455c5e64
Raymond TeHok <rth@mydatamodels.com>
2022-08-08 10:18:09 +0200

    Add https://leetcode.com/problems/longest-increasing-subsequence

---
+++ leetcode/300_longest-increasing-subsequence.py
@@ -0,0 +1,37 @@
+from typing import List
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        res = []
+
+        # Look into res the index to replace
+        def binarySearch(l, r, target):
+            mid = (r - l) // 2 + l
+
+            if l == r:
+                return l
+
+            if res[mid] == target:
+                return mid
+            elif res[mid] < target:
+                return binarySearch(mid + 1, r, target)
+            else:
+                return binarySearch(l, mid, target)  # do not decrease right, because it might be in the solution
+
+        for num in nums:
+            if not res or res[-1] < num:
+                res.append(num)
+                continue
+
+            i = binarySearch(0, len(res) - 1, num)
+
+            res[i] = num
+
+        return len(res)
+
+
+if __name__ == "__main__":
+    assert Solution().lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]) == 4
+    assert Solution().lengthOfLIS([0, 1, 0, 3, 2, 3]) == 4
+    assert Solution().lengthOfLIS([7, 7, 7, 7, 7, 7, 7]) == 1
