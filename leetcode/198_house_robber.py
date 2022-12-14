6c2cd0ef6e74c08864000be5f31ed6008ae2c5a6
Raymond TeHok <rth@mydatamodels.com>
2022-12-14 09:09:10 +0100

    Update https://leetcode.com/problems/house-robber

--- leetcode/198_house_robber.py
+++ leetcode/198_house_robber.py
@@ -25,6 +25,32 @@ class Solution:
 
         return m1
 
+    def rob_mutate(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n <= 2:
+            return max(nums)
+
+        for i in range(2, n):
+            nums[i] = max(nums[i - 1], nums[i - 2] + nums[i])
+            nums[i - 1] = max(nums[i - 1], nums[i - 2])
+
+        return nums[-1]
+
+    def rob_sub_optimal(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n <= 2:
+            return max(nums)
+
+        h_1 = nums[1]  # house - 1
+        h_2 = nums[0]  # house - 2
+
+        for i in range(2, n):
+            curr = max(h_1, h_2 + nums[i])
+            h_2 = max(h_1, h_2)
+            h_1 = curr
+
+        return curr
+
 
 if __name__ == "__main__":
     assert Solution().rob([2, 7, 9, 3, 1]) == 12
