24f839224e8402d3030d4db9aff61fb8d18e8f98
Raymond Te Hok <tehok.raymond@gmail.com>
2023-04-19 17:04:14 +0200

    Update https://leetcode.com/problems/search-in-rotated-sorted-array

--- leetcode/33_search_in_rotated_sorted_array.py
+++ leetcode/33_search_in_rotated_sorted_array.py
@@ -30,12 +30,12 @@ class Solution:
                 return mid
 
             if nums[left] <= nums[mid]:  # left half is sorted
-                if nums[left] <= target and target < nums[mid]:
+                if nums[left] <= target < nums[mid]:  # is in left side
                     right = mid - 1
                 else:
                     left = mid + 1
             else:  # right side is sorted
-                if nums[right] < target and target < nums[mid]:
+                if nums[mid] < target <= nums[right]:  # is in right side
                     left = mid + 1
                 else:
                     right = mid - 1
@@ -47,3 +47,4 @@ if __name__ == "__main__":
     assert Solution().search([4, 5, 6, 7, 0, 1, 2], 0) == 4
     assert Solution().search([4, 5, 6, 7, 0, 1, 2], 3) == -1
     assert Solution().search([1], 0) == -1
+    assert Solution().search([5, 1, 3], 3) == 2
