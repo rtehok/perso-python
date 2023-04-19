bd50929706a40dff9de92038b09bcdf146826ec7
Raymond Te Hok <tehok.raymond@gmail.com>
2023-04-19 13:08:25 +0200

    Add tree helpers

---
+++ leetcode/helpers/helpers.py
@@ -0,0 +1,31 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def build_tree(lst):
+    if not lst:
+        return None
+
+    root = TreeNode(lst[0])
+    queue = [root]
+    i = 1
+
+    while i < len(lst):
+        node = queue.pop(0)
+
+        if lst[i] is not None:
+            node.left = TreeNode(lst[i])
+            queue.append(node.left)
+
+        i += 1
+
+        if i < len(lst) and lst[i] is not None:
+            node.right = TreeNode(lst[i])
+            queue.append(node.right)
+
+        i += 1
+
+    return root
