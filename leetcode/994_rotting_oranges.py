5a10663e54e24f450b79bedca3f06b5d6fc50bcb
Raymond Te Hok <tehok.raymond@gmail.com>
2023-04-21 11:47:42 +0200

    Update https://leetcode.com/problems/rotting-oranges

--- leetcode/994_rotting_oranges.py
+++ leetcode/994_rotting_oranges.py
@@ -1,42 +1,55 @@
-from collections import deque
+import collections
 from typing import List
 
 
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
-        moves = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        moves = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        m, n = len(grid), len(grid[0])
+        minutes = 0
+        fresh = 0
 
-        q = deque()
+        q = collections.deque()
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 2:
                     q.append((i, j))
+                if grid[i][j] == 1:
+                    fresh += 1
 
-        time = 0
+        if fresh == 0:
+            return 0
 
         while q:
-            for i in range(len(q)):
-                r, c = q.popleft()
-                for dr, dc in moves:
-                    if 0 <= r + dr <= m - 1 and 0 <= c + dc <= n - 1 and grid[r + dr][c + dc] == 1:
-                        grid[r + dr][c + dc] = 2
-                        q.append((r + dr, c + dc))
+            q_len = len(q)
+            for _ in range(q_len):
+                x, y = q.popleft()
+                for dx, dy in moves:
+                    nx, ny = x + dx, y + dy
+                    if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] == 1:
+                        grid[nx][ny] = 2
+                        fresh -= 1
+                        q.append((nx, ny))
 
-            time += 1
+            minutes += 1
 
-        for row in grid:
-            for orange in row:
-                if orange == 1:
-                    return -1
+        if fresh > 0:
+            return -1
 
-        return time - 1 if time > 0 else time
+        return minutes - 1
 
 
 if __name__ == "__main__":
     assert Solution().orangesRotting([[2, 1, 1], [1, 1, 0], [0, 1, 1]]) == 4
-    assert Solution().orangesRotting([[2, 1, 1],
-                                      [0, 1, 1],
-                                      [1, 0, 1]]) == -1
+    assert Solution().orangesRotting([
+        [2, 1, 1],
+        [0, 1, 1],
+        [1, 0, 1]
+    ]) == -1
     assert Solution().orangesRotting([[0, 2]]) == 0
+    assert Solution().orangesRotting([[0]]) == 0
+    assert Solution().orangesRotting([[0, 2, 2]]) == 0
+    assert Solution().orangesRotting([
+        [2, 1, 1],
+        [1, 1, 1],
+        [0, 1, 2]]) == 2
