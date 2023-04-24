ef650756eae33950ca60f4d86cc90ca93e312ae2
Raymond TeHok <tehok.raymond@gmail.com>
2023-04-24 09:41:57 +0200

    Update https://leetcode.com/problems/last-stone-weight

--- leetcode/1046_last_stone_weight.py
+++ leetcode/1046_last_stone_weight.py
@@ -1,8 +1,9 @@
+import heapq
 from typing import List
 
 
 class Solution:
-    def lastStoneWeight(self, stones: List[int]) -> int:
+    def lastStoneWeightV1(self, stones: List[int]) -> int:
         stones.sort(reverse=True)
         while len(stones) > 1:
             if stones[0] == stones[1]:
@@ -15,6 +16,19 @@ class Solution:
                 stones.sort(reverse=True)
 
         return stones[0]
+
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        h = []
+        for stone in stones:
+            heapq.heappush(h, -stone)
+
+        while len(h) > 1:
+            stone1 = heapq.heappop(h)
+            stone2 = heapq.heappop(h)
+            if stone1 != stone2:
+                heapq.heappush(h, stone1 - stone2)
+
+        return 0 if not h else -heapq.heappop(h)
 
 
 if __name__ == "__main__":
