5bbcd8f77a312059c0df36d28072b3f65ee625e6
Raymond TeHok <tehok.raymond@gmail.com>
2023-04-27 21:15:00 +0200

    Add https://leetcode.com/problems/min-stack

---
+++ leetcode/155_min_stack.py
@@ -0,0 +1,56 @@
+class MinStackV1:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        if self.stack[-1] == self.min_stack[-1]:
+            self.min_stack.pop()
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+
+
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.min = float('inf')
+
+    def push(self, val: int) -> None:
+        if val <= self.min:
+            self.stack.append(self.min)
+            self.min = val
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        if self.stack[-1] == self.min:
+            self.stack.pop()
+            self.min = self.stack.pop()
+        else:
+            self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min
+
+
+if __name__ == "__main__":
+    s = MinStack()
+    s.push(-2)
+    s.push(0)
+    s.push(-3)
+    assert s.getMin() == -3
+    s.pop()
+    assert s.top() == 0
+    assert s.getMin() == -2
