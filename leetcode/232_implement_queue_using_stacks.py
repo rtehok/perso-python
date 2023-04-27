4b843ea696a3f8262f162e26c66c6d56770c4f12
Raymond Te Hok <tehok.raymond@gmail.com>
2023-04-27 12:01:44 +0200

    Update https://leetcode.com/problems/implement-queue-using-stacks

--- leetcode/232_implement_queue_using_stacks.py
+++ leetcode/232_implement_queue_using_stacks.py
@@ -1,6 +1,3 @@
-from collections import deque
-
-
 class MyQueue:
 
     def __init__(self):
@@ -25,9 +22,11 @@ class MyQueue:
     def empty(self) -> bool:
         return not len(self.stack_in)
 
-# Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.peek()
-# param_4 = obj.empty()
+
+if __name__ == "__main__":
+    q = MyQueue()
+    q.push(1)
+    q.push(2)
+    assert q.peek() == 1
+    assert q.pop() == 1
+    assert not q.empty()
