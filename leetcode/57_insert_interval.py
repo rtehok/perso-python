20b151bb470ffb72e2d5b38f97bb5e7d9db3a8be
Raymond Te Hok <tehok.raymond@gmail.com>
2023-04-28 17:30:05 +0200

    Update https://leetcode.com/problems/insert-interval

--- leetcode/57_insert_interval.py
+++ leetcode/57_insert_interval.py
@@ -2,21 +2,23 @@ from typing import List
 
 
 class Solution:
-    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+    def insert(self, intervals: List[List[int]], new_interval: List[int]) -> List[List[int]]:
         res = []
-        for i in range(len(intervals)):
-            if newInterval[1] < intervals[i][0]:
-                res.append(newInterval)
+        for i, (start, end) in enumerate(intervals):
+            new_start, new_end = new_interval[0], new_interval[1]
+            if new_end < start:
+                res.append(new_interval)
                 return res + intervals[i:]
-            elif newInterval[0] > intervals[i][1]:
-                res.append(intervals[i])
+            elif new_start > end:
+                res.append([start, end])
             else:
-                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+                new_interval = [min(new_start, start), max(new_end, end)]
 
-        res.append(newInterval)
+        res.append(new_interval)
         return res
 
 
 if __name__ == "__main__":
-    print(Solution().insert([[1, 3], [6, 9]], [2, 5]))
-    print(Solution().insert([[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]], [4, 8]))
+    assert Solution().insert([], [5, 7]) == [[5, 7]]
+    assert Solution().insert([[1, 3], [6, 9]], [2, 5]) == [[1, 5], [6, 9]]
+    assert Solution().insert([[1, 2], [3, 5], [6, 7], [8, 10], [12, 16]], [4, 8]) == [[1, 2], [3, 10], [12, 16]]
