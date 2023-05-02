6d834aef967ccdcaae2ba0ae0d9c908c3c9594e6
Raymond Te Hok <tehok.raymond@gmail.com>
2023-05-02 13:01:55 +0200

    Add is_same_tree

--- leetcode/helpers/helpers.py
+++ leetcode/helpers/helpers.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -29,3 +32,14 @@ def build_tree(lst):
         i += 1
 
     return root
+
+
+def is_same_tree(p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+    if not p and not q:
+        return True
+    elif not p or not q:
+        return False
+    elif p.val != q.val:
+        return False
+    else:
+        return is_same_tree(p.left, q.left) and is_same_tree(p.right, q.right)
