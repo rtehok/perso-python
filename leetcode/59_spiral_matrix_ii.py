a40f99f1cb17aa7462a4460b28ad0c8fd6ff8297
Raymond Te Hok <tehok.raymond@gmail.com>
2023-05-10 11:50:38 +0200

    Update https://leetcode.com/problems/spiral-matrix-ii

--- leetcode/59_spiral_matrix_ii.py
+++ leetcode/59_spiral_matrix_ii.py
@@ -1,58 +1,65 @@
+import math
 from typing import List
 
 
 class Solution:
+    def generateMatrixV1(self, n: int) -> List[List[int]]:
+        ans = [[0] * n for _ in range(n)]
+        right, down = 1, 0
+        limit_left, limit_right = 0, n - 1
+        limit_up, limit_down = 0, n - 1
+        row, col = 0, 0
+        i = 1
+        while limit_left <= limit_right and limit_up <= limit_down:
+            ans[row][col] = i
+
+            if right == 1 and col == limit_right:
+                right = 0
+                down = 1
+                limit_up += 1
+
+            if down == 1 and row == limit_down:
+                right = -1
+                down = 0
+                limit_right -= 1
+
+            if right == -1 and col == limit_left:
+                right = 0
+                down = -1
+                limit_down -= 1
+
+            if down == -1 and row == limit_up:
+                right = 1
+                down = 0
+                limit_left += 1
+
+            col += right
+            row += down
+
+            i += 1
+
+        return ans
+
     def generateMatrix(self, n: int) -> List[List[int]]:
-        matrix = [[0] * n for _ in range(n)]
+        ans = [[0] * n for _ in range(n)]
+        cnt = 1
+        moves = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+        direction = 0
+        row, col = 0, 0
+        while cnt <= n ** 2:
+            ans[row][col] = cnt
+            cnt += 1
+            r = (row + moves[direction][0]) % n  # next row
+            c = (col + moves[direction][1]) % n  # next col
 
-        start = 0
-        i = 0
-        j = 0
-        up, down, right, left = 0, n, n, -1
+            if ans[r][c] != 0:
+                direction = (direction + 1) % 4
 
-        direction = "right"
+            row += moves[direction][0]
+            col += moves[direction][1]
 
-        while start < n * n:
-            if direction == "right":
-                while j < right and start != n * n:
-                    matrix[i][j] = start + 1
-                    j += 1
-                    start += 1
-                direction = "down"
-                right -= 1
-                j -= 1
-                i += 1
-            if direction == "down":
-                while i < down and start != n * n:
-                    matrix[i][j] = start + 1
-                    i += 1
-                    start += 1
-                direction = "left"
-                down -= 1
-                i -= 1
-                j -= 1
-            if direction == "left":
-                while j > left and start != n * n:
-                    matrix[i][j] = start + 1
-                    j -= 1
-                    start += 1
-                direction = "up"
-                left += 1
-                j += 1
-                i -= 1
-            if direction == "up":
-                while i > up and start != n * n:
-                    matrix[i][j] = start + 1
-                    i -= 1
-                    start += 1
-                direction = "right"
-                up += 1
-                i += 1
-                j += 1
-
-        return matrix
+        return ans
 
 
 if __name__ == "__main__":
-    # print(Solution().generateMatrix(3))
-    print(Solution().generateMatrix(5))
+    assert Solution().generateMatrix(3) == [[1, 2, 3], [8, 9, 4], [7, 6, 5]]
