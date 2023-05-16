e2af06c835f9457d0de3bd50056363f69e079c40
Raymond TeHok <tehok.raymond@gmail.com>
2023-05-16 11:10:40 +0200

    Add https://leetcode.com/problems/swap-nodes-in-pairs/

--- leetcode/24_swap_nodes_in_pairs.py
+++ leetcode/24_swap_nodes_in_pairs.py
@@ -1,11 +1,14 @@
 # Definition for singly-linked list.
 from typing import Optional
 
+from leetcode.helpers.helpers import build_list, compare_linked_lists
+
 
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
 
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
@@ -22,3 +25,10 @@ class Solution:
         head.next.next = self.swapPairs(third)
 
         return head
+
+
+if __name__ == "__main__":
+    assert compare_linked_lists(Solution().swapPairs(build_list([1, 2, 3, 4])),
+                                build_list([2, 1, 4, 3]))
+    assert compare_linked_lists(Solution().swapPairs(build_list([])),
+                                build_list([]))
