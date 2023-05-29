d63ed4141704c4634f37e05672fc76b4c0bb23c8
Raymond Te Hok <tehok.raymond@gmail.com>
2023-05-29 17:02:13 +0200

    Update https://leetcode.com/problems/remove-duplicates-from-sorted-array/

--- leetcode/26_remove_duplicates_from_sorted_array.py
+++ leetcode/26_remove_duplicates_from_sorted_array.py
@@ -11,3 +11,11 @@ class Solution:
                 nums[i] = nums[j]
         return i + 1
 
+
+if __name__ == "__main__":
+    a = [1, 1, 2]
+    assert Solution().removeDuplicates(a) == 2
+    assert a[:2] == [1, 2]
+    a = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+    assert Solution().removeDuplicates(a) == 5
+    assert a[:5] == [0, 1, 2, 3, 4]
