863cda320d53564ef4ea4494e36a4e7ac4ebc4b0
Raymond TeHok <tehok.raymond@gmail.com>
2023-05-30 23:53:03 +0200

    Update https://leetcode.com/problems/rotate-array

--- leetcode/189_rotate_array.py
+++ leetcode/189_rotate_array.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 class Solution:
-    def rotate(self, nums: List[int], k: int) -> None:
+    def rotateV1(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
@@ -12,6 +12,27 @@ class Solution:
         n = len(nums)
         k = k % n
         nums[k:], nums[:k] = nums[:n - k], nums[n - k:]
+
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        if k == 0:
+            return
+
+        n = len(nums)
+        k %= n  # handle k > n
+
+        def reverse(start, end):
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+
+        # 1 2 3 4 5 6 7
+        reverse(0, n - 1)  # 7 6 5 4 3 2 1
+        reverse(0, k - 1)  # 5 6 7 - - - -
+        reverse(k, n - 1)  # - - - 1 2 3 4
 
 
 if __name__ == "__main__":
