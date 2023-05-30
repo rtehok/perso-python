ce40ebf72e2b0bfd205fbcc3a01b97ba96f5d216
Raymond Te Hok <tehok.raymond@gmail.com>
2023-05-30 16:08:54 +0200

    Add https://leetcode.com/problems/majority-element

---
+++ leetcode/169_majority_element.py
@@ -0,0 +1,36 @@
+from typing import List
+
+
+class SolutionV1:
+    def majorityElement(self, nums: List[int]) -> int:
+        nums.sort()
+        majority, maj_elt = 0, None
+        cnt = 0
+        for i, x in enumerate(nums):
+            if i > 0 and nums[i - 1] < nums[i]:
+                cnt = 0
+
+            cnt += 1
+            if majority < cnt:
+                majority = cnt
+                maj_elt = x
+
+        return maj_elt
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        candidate = 0
+        count = 0
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += 1 if candidate == num else -1
+
+        return candidate
+
+
+if __name__ == "__main__":
+    assert Solution().majorityElement([3, 2, 3]) == 3
+    assert Solution().majorityElement([2, 2, 1, 1, 1, 2, 2]) == 2
