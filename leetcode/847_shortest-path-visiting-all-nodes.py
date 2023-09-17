f3c1f0492e08947a20e89b8e973dbfea45c6a9a1
Raymond TeHok <tehok.raymond@gmail.com>
2023-09-17 14:23:41 +0200

    Add https://leetcode.com/problems/shortest-path-visiting-all-nodes

---
+++ leetcode/847_shortest-path-visiting-all-nodes.py
@@ -0,0 +1,39 @@
+import collections
+from typing import List
+
+
+class Solution:
+    def shortestPathLength(self, graph: List[List[int]]) -> int:
+        n = len(graph)
+
+        all_mask = (1 << n) - 1
+        visited = set()
+        Node = collections.namedtuple("Node", ["node", "mask", "cost"])
+        q = collections.deque()
+
+        for i in range(n):
+            mask_value = (1 << i)
+            this_node = Node(i, mask_value, 1)
+            q.append(this_node)
+            visited.add((i, mask_value))
+
+        while q:
+            curr = q.popleft()
+
+            if curr.mask == all_mask:
+                return curr.cost - 1
+
+            for adj in graph[curr.node]:
+                both_visited_mask = curr.mask | (1 << adj)
+                this_node = Node(adj, both_visited_mask, curr.cost + 1)
+
+                if (adj, both_visited_mask) not in visited:
+                    visited.add((adj, both_visited_mask))
+                    q.append(this_node)
+
+        return -1
+
+
+if __name__ == "__main__":
+    assert Solution().shortestPathLength([[1, 2, 3], [0], [0], [0]]) == 4
+    assert Solution().shortestPathLength([[1], [0, 2, 4], [1, 3, 4], [2], [1, 2]]) == 4
