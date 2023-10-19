81c51d9a2161adb9edb83226109e5f1a60851434
Raymond Te Hok <tehok.raymond@gmail.com>
2023-10-19 12:40:46 +0200

    Update https://leetcode.com/problems/backspace-string-compare

--- leetcode/844_backspace-string_compare.py
+++ leetcode/844_backspace-string_compare.py
@@ -2,25 +2,57 @@ import itertools
 
 
 class Solution:
+    def backspaceCompareV1(self, s: str, t: str) -> bool:
+        stack_1 = []
+
+        i = 0
+        while i < len(s):
+            if s[i] != "#":
+                stack_1.append(s[i])
+                i += 1
+                continue
+
+            while i < len(s) and s[i] == "#":
+                if stack_1:
+                    stack_1.pop()
+                i += 1
+
+        stack_2 = []
+        j = 0
+        while j < len(t):
+            if t[j] != "#":
+                stack_2.append(t[j])
+                j += 1
+                continue
+
+            while j < len(t) and t[j] == "#":
+                if stack_2:
+                    stack_2.pop()
+                j += 1
+
+        return stack_1 == stack_2
+
+    """O(M + N) / O(M + N)"""
+    def backspaceCompareV2(self, s: str, t: str) -> bool:
+        stack_1 = []
+        stack_2 = []
+
+        for c in s:
+            if c == '#' and stack_1:
+                stack_1.pop()
+            elif c != '#':
+                stack_1.append(c)
+
+        for c in t:
+            if c == '#' and stack_2:
+                stack_2.pop()
+            elif c != '#':
+                stack_2.append(c)
+
+        return stack_1 == stack_2
+
+    """O(M + N) / O(1)"""
     def backspaceCompare(self, s: str, t: str) -> bool:
-        """O(M + N) / O(M + N)"""
-        # stack_1 = []
-        # stack_2 = []
-        #
-        # for c in s:
-        #     if c == '#' and stack_1:
-        #         stack_1.pop()
-        #     elif c != '#':
-        #         stack_1.append(c)
-        #
-        # for c in t:
-        #     if c == '#' and stack_2:
-        #         stack_2.pop()
-        #     elif c != '#':
-        #         stack_2.append(c)
-        #
-        # return stack_1 == stack_2
-        """O(M + N) / O(1)"""
         def helper(ss):
             skip = 0
             for c in reversed(ss):
