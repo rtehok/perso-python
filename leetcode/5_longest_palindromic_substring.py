d6c9ed012e5efa220a22c120d7fc073b1e9cf108
Raymond Te Hok <tehok.raymond@gmail.com>
2023-10-27 11:52:41 +0200

    Update https://leetcode.com/problems/longest-palindromic-substring

--- leetcode/5_longest_palindromic_substring.py
+++ leetcode/5_longest_palindromic_substring.py
@@ -1,5 +1,5 @@
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
+    def longestPalindromeV1(self, s: str) -> str:
         self.start = 0
         self.length = 0
 
@@ -17,7 +17,30 @@ class Solution:
 
         return s[self.start:self.start + self.length]
 
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        ans = [0, 0]
+
+        for i in range(n):
+            dp[i][i] = True
+
+        for i in range(n - 1):
+            if s[i] == s[i + 1]:
+                dp[i][i + 1] = True
+                ans = [i, i + 1]
+
+        for diff in range(2, n):
+            for i in range(n - diff):
+                j = i + diff
+                if s[i] == s[j] and dp[i + 1][j - 1]:
+                    dp[i][j] = True
+                    ans = [i, j]
+
+        i, j = ans
+        return s[i:j + 1]
+
 
 if __name__ == "__main__":
-    print(Solution().longestPalindrome("babad"))
-    print(Solution().longestPalindrome("cbbd"))
+    assert Solution().longestPalindrome("babad") == "aba"
+    assert Solution().longestPalindrome("cbbd") == "bb"
