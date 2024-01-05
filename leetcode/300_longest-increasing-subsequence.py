47f29f6fedd96e3e7d8c6908c33669434a7396b0
Raymond TeHok <tehok.raymond@gmail.com>
2024-01-05 15:26:36 +0100

    Update https://leetcode.com/problems/longest-increasing-subsequence

--- leetcode/300_longest-increasing-subsequence.py
+++ leetcode/300_longest-increasing-subsequence.py
@@ -3,6 +3,19 @@ from typing import List
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        n = len(nums)
+        dp = [1] * n
+        for i in range(1, n):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+
+        return dp[-1]
+
+    def lengthOfLISV2(self, nums: List[int]) -> int:
         res = []
 
         # Look into res the index to replace
