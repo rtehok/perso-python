f24d4c0b75aa8b71f9281086d0d24bd2af0b8ea1
Raymond TeHok <tehok.raymond@gmail.com>
2024-02-06 12:01:59 +0100

    Update https://leetcode.com/problems/first-unique-character-in-a-string

--- leetcode/387_first_unique_character_in_a_string.py
+++ leetcode/387_first_unique_character_in_a_string.py
@@ -2,15 +2,27 @@ from collections import defaultdict
 
 
 class Solution:
-    def firstUniqChar(self, s: str) -> int:
-        d = defaultdict(list)
+    def firstUniqCharV1(self, s: str) -> int:
+        d = {}
+        for c in s:
+            if c in d:
+                d[c] += 1
+            else:
+                d[c] = 1
+
         for i, c in enumerate(s):
-            d[c].append(i)
+            if d[c] == 1:
+                return i
 
-        for k, v in d.items():
-            if len(v) == 1:
-                return v[0]
+        return -1
 
+    def firstUniqChar(self, s: str) -> int:
+        cnt = [0] * 26
+        for i in range(len(s)):
+            cnt[ord(s[i]) - ord("a")] += 1
+        for i in range(len(s)):
+            if cnt[ord(s[i]) - ord("a")] == 1:
+                return i
         return -1
 
 
