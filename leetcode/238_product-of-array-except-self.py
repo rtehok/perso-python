bf00922464d106ea41a09b0e2873b8780fd8dd94
Raymond Te Hok <tehok.raymond@gmail.com>
2024-03-15 09:42:27 +0100

    Add https://leetcode.com/problems/product-of-array-except-self

---
+++ leetcode/238_product-of-array-except-self.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        total = 1
+
+        for num in nums:
+            total *= num
+
+        ans = []
+
+        for i, num in enumerate(nums):
+            if num != 0:
+                ans.append(total // num)
+            else:
+                tmp = 1
+                for left in nums[:i]:
+                    tmp *= left
+                for right in nums[i + 1:]:
+                    tmp *= right
+                ans.append(tmp)
+
+        return ans
+
+
+assert Solution().productExceptSelf(nums=[1, 2, 3, 4]) == [24, 12, 8, 6]
+assert Solution().productExceptSelf(nums=[-1, 1, 0, -3, 3]) == [0, 0, 9, 0, 0]
